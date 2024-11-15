259180ff1d4c5905528dd8dc15820d2e36233332
José Venício Lira Rodrigues <109315971+veniciolira@users.noreply.github.com>
2024-11-15 19:30:46 -0300

    Update questao2.java

--- PLE/src/main/java/org/example/questao2.java
+++ PLE/src/main/java/org/example/questao2.java
@@ -1,17 +1,25 @@
-package org.example;
+import java.util.Arrays;
 
 public class questao2 {
+    static class Item {
+        int valor, peso;
+
+        Item(int valor, int peso) {
+            this.valor = valor;
+            this.peso = peso;
+        }
+    }
+
     public static int knapSack(int W, int wt[], int val[], int n) {
         int i, w;
         int K[][] = new int[n+1][W+1];
 
-        // Build table K[][] in bottom up manner
         for (i = 0; i <= n; i++) {
             for (w = 0; w <= W; w++) {
-                if (i==0 || w==0)
+                if (i == 0 || w == 0)
                     K[i][w] = 0;
                 else if (wt[i-1] <= w)
-                    K[i][w] = Math.max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+                    K[i][w] = Math.max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]);
                 else
                     K[i][w] = K[i-1][w];
             }
@@ -19,3 +27,32 @@ public class questao2 {
 
         return K[n][W];
     }
+
+    public static double knapSackGuloso(int W, int[] val, int[] wt) {
+        int n = val.length;
+        Item[] itens = new Item[n];
+
+        for (int i = 0; i < n; i++) {
+            itens[i] = new Item(val[i], wt[i]);
+        }
+
+        Arrays.sort(itens, (a, b) -> Double.compare((double) b.valor / b.peso, (double) a.valor / a.peso));
+
+        double valorTotal = 0.0;
+        int capacidadeRestante = W;
+
+        for (Item item : itens) {
+            if (capacidadeRestante == 0) break;
+
+            if (item.peso <= capacidadeRestante) {
+                capacidadeRestante -= item.peso;
+                valorTotal += item.valor;
+            } else {
+                valorTotal += (double) item.valor * capacidadeRestante / item.peso;
+                capacidadeRestante = 0;
+            }
+        }
+
+        return valorTotal;
+    }
+}
