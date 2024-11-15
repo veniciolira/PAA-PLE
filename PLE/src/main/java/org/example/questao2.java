af7d94e0642aa2908eba9bb60f50949324feee96
José Venício Lira Rodrigues <109315971+veniciolira@users.noreply.github.com>
2024-11-15 18:56:45 -0300

    Update questao2.java

--- PLE/src/main/java/org/example/questao2.java
+++ PLE/src/main/java/org/example/questao2.java
@@ -19,12 +19,3 @@ public class questao2 {
 
         return K[n][W];
     }
-
-    public static void main(String args[]) {
-        int val[] = new int[]{60, 100, 120};
-        int wt[] = new int[]{10, 20, 30};
-        int W = 50;
-        int n = val.length;
-        System.out.println(knapSack(W, wt, val, n));
-    }
-}
